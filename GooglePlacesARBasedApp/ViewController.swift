8a5d841825128a420721ed7890508d553ecf3ab9
erkutbas <ebas1@binghamton.edu>
2019-04-18 01:43:15 +0300

    Initial Commit

---
+++ GooglePlacesARBasedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GooglePlacesARBasedApp
+//
+//  Created by Erkut Baş on 4/18/19.
+//  Copyright © 2019 Erkut Baş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
